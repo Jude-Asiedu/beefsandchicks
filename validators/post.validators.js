933854e7f88cf18a34d1afd416a44d33c6d8aa14
Jude Asiedu <jude.asiedu99@gmail.com>
2025-10-01 19:22:26 +0000

    Updated validators for request

--- validators/post.validators.js
+++ validators/post.validators.js
@@ -2,10 +2,11 @@
 module.exports = {
   createPost: {
     title: { type: "string", optional: false, max: 100 },
-    content: { type: "string", optional: false, max: 5000 }
+    content: { type: "string", optional: false, max: 5000 },
+    image_url: { type: "string", optional: false, max: 100 }
   },
   updatePost: {
-    title: { type: "string", optional: true, max: 100 },
+    title: { type: "string", optional: false, max: 100 },
     content: { type: "string", optional: true, max: 5000 }
   },
   postIdParam: {
--- validators/user.validators.js
+++ validators/user.validators.js
@@ -4,5 +4,9 @@ module.exports ={
         name:{type: "string", optional: false, max: 50},
         password:{type: "string", optional: false, max: 100},
         email:{type: "email", optional: false, max: 100}
+    },
+    login:{
+        password:{type: "string", optional: false, max: 100},
+        email:{type: "email", optional: false, max: 100}
     }
 }
